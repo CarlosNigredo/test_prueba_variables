e0979b7b307825ccd70ee482da4185cacde30788
Carlos Nigredo <carlos.nigredo@gmail.com>
2022-08-10 16:25:57 -0500

    Ensayos de práctica
    Se agregaron los ensayos de práctica, los datos para estos ensayos están en un js independiente. Se creo la carpeta de archivos js

---
+++ js/data_practica.js
@@ -0,0 +1,58 @@
+var dataPractica = [
+    {
+        variable: 'peso',
+        modelo: 80,
+        comparacion: 140,
+        css: 'wght',
+        css2: "'wdth' 700, 'slnt' 0",
+    },
+    {
+        variable: 'ancho',
+        comparacion: 900,
+        modelo: 600,
+        css: 'wdth',
+        css2: "'wght' 110, 'slnt' 0",
+    },
+    {
+        variable: 'inclinación',
+        modelo: 7,
+        comparacion: 20,
+        css: 'slnt',
+        css2: "'wdth' 700, 'wght' 110",
+    },
+    {
+        variable: 'inclinación',
+        modelo: -7,
+        comparacion: -20,
+        css: 'slnt',
+        css2: "'wdth' 700, 'wght' 110",
+    },
+    {
+        variable: 'peso',
+        modelo: 90,
+        comparacion: 120,
+        css: 'wght',
+        css2: "'wdth' 700, 'slnt' 0",
+    },
+    {
+        variable: 'ancho',
+        comparacion: 850,
+        modelo: 650,
+        css: 'wdth',
+        css2: "'wght' 110, 'slnt' 0",
+    },
+    {
+        variable: 'inclinación',
+        modelo: 10,
+        comparacion: 20,
+        css: 'slnt',
+        css2: "'wdth' 700, 'wght' 110",
+    },
+    {
+        variable: 'inclinación',
+        modelo: -10,
+        comparacion: -20,
+        css: 'slnt',
+        css2: "'wdth' 700, 'wght' 110",
+    }
+]
